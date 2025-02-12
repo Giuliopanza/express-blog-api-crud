4d5f7c883b62e78b558c1bb4f9a1a73559d67bc1
Giulio <giuliopanza2000@gmail.com>
2025-02-12 17:45:15 +0100

    remove posts routers

--- routers/routers.js
+++ routers/routers.js
@@ -1,8 +1,6 @@
 const express = require('express')
 
 const router = express.Router();
-
-const arrayPosts = require('../data/posts.js');
 	
 // const pizzaController = require('../controllers/pizzaController');
 
